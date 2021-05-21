1d58c456ea4f29b5d417017be3c2ad716c736d5a
jhm <zttjhm@163.com>
2021-05-21 08:24:18 +0800

    初始化

---
+++ MobileDataDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MobileDataDemo
+//
+//  Created by jhm on 2021/5/21.
+//  Copyright © 2021 jhm. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
